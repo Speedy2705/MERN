b65f5d5e6259d4989aa71d2999b7c83ed2e489bb
Speedy2705 <163318944+Speedy2705@users.noreply.github.com>
2024-07-11 13:17:20 +0530

    Commit All

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require ('express')
+const cors = require ('cors')
+require('dotenv').config()
+const connectDB = require('./config/db')
+
+
+const app=express()
+app.use(cors())
+
+
+const PORT = 8000 || process.env.PORT
+
+
+connectDB().then (()=>{
+    app.listen(PORT,()=>{
+        console.log("Connect to DB");
+        console.log("Server is running !!!!!!!");
+    })
+})
